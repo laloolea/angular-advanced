acd7bf969a3bc5d69a61ec0dcf6741762b29e836
Eduardo Olea <laloolea18@gmail.com>
2022-02-18 12:18:11 -0700

    Finished section 7 about basic services, themes, routes and persistence

--- 03-adminpro/src/app/pages/pages.module.ts
+++ 03-adminpro/src/app/pages/pages.module.ts
@@ -9,6 +9,7 @@ import { PagesComponent } from './pages.component';
 import { ProgressComponent } from './progress/progress.component';
 import { SharedModule } from '../shared/shared.module';
 import { ComponentsModule } from '../components/components.module';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 
@@ -18,13 +19,15 @@ import { ComponentsModule } from '../components/components.module';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   exports:[
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   imports: [
     CommonModule,
